00ba2f20bdd5b0a1a6f9e85d55279cf1400fe7aa
cpk <cpk0709@gmail.com>
2022-03-08 13:56:20 +0900

    add editModal in main.html

--- static/main.js
+++ static/main.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         ],
         dateClick: function (info) {
-
+            //Modal 띄워주는 처리
             const dayTag = document.getElementById("selec_day");
             //클릭한 날짜를 모달 날짜태그에 넣어줌
             dayTag.innerText = info.dateStr;
@@ -66,9 +66,11 @@ document.addEventListener('DOMContentLoaded', function () {
             const modal = document.querySelector(".modal.enter");
             const overlay = modal.querySelector(".modal__overlay");
             const closeBtn = modal.querySelector("#close_btn");
+            //modal의 class="hidden"을 삭제
             const openModal = () => {
                 modal.classList.remove("hidden");
             }
+            //modal의 class="hidden"을 추가가
             const closeModal = () => {
                 modal.classList.add("hidden");
             }
@@ -78,12 +80,42 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         //여기서 수정/삭제처리할 예정
         eventClick: function (info) {
-            alert('Event: ' + info.event.title);
+            //클릭한 일정의 시작날짜데이터를 dateInfo에 담아주고있음
+            const dateInfo = info.el.fcSeg.eventRange.range.start;
+            //자바스크립트 Date함수를 통해 date 인스턴스 생성
+            const date = new Date(dateInfo);
+            //오브젝트탑인인 date를 string으로 바꾼 후 split으로 나누어 배열로 만들고있음.
+            const dateArray = String(date).split(" ");
+            //dateArray의 year부분
+            const year = dateArray[3];
+            //dateArray의 month부분. month는 0부터시작이라 +1해줌. 두자리로 만들기위해 '0'붙임
+            const month = '0' + (date.getMonth(dateArray[1])+1);
+            //dateArray의 day부분
+            const day = dateArray[2];
 
-
-            const modal = document.querySelector(".modal.enter");
+            const editDate = year.concat('-',month,'-',day);
+            //스케줄 시작일을 수정/삭제 모달에 넣어주고있다.
+            document.getElementById('edit_day').innerText = editDate;
+            //스케줄 내용을 수정/삭제 모달에 넣어주고있다.
+            document.querySelector("#edit_input").innerText = info.event.title;
+            const modal = document.querySelector(".modal.remove");
             const overlay = modal.querySelector(".modal__overlay");
             const closeBtn = modal.querySelector("#close_btn");
+            //modal의 class="hidden"을 삭제
+            const openModal = () => {
+                modal.classList.remove("hidden");
+            }
+            //modal의 class="hidden"을 추가가
+            const closeModal = () => {
+                modal.classList.add("hidden");
+            }
+
+            overlay.addEventListener("click", closeModal);
+            closeBtn.addEventListener("click", closeModal);
+
+            openModal();
+
+
             // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
 
             // change the border color just for fun
